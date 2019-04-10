1f18ed683f3a289199c8c4e593e86d4dc0854a51
andy911002 <48443745+andy911002@users.noreply.github.com>
2019-04-10 19:45:13 +0800

    Create 2019010  .cpp

---
+++ 2019010  .cpp
@@ -0,0 +1,15 @@
+#include  <iostream>  
+  
+using namespace std;  
+int main()  
+{  
+int A,B,C;  
+cout<<"Chinese :";
+cin>>A;
+cout<<"Math :";
+cin>>B;
+cout<<"English :";
+cin>>C;
+cout<<"Total score :"<<  A+B+C<< endl;  
+cout<<"average :"<<  (A+B+C)/3;   
+}
