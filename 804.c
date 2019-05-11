c6dacd263620e85a43352da49e9a132bc6ae149f
andy911002 <48443745+andy911002@users.noreply.github.com>
2019-05-11 22:58:51 +0800

    Update 804.c

--- 804.c
+++ 804.c
@@ -2,30 +2,25 @@
 #include <stdlib.h>
 #define MAX 6
 
-
 double average(double a[]){
     int i;
     double sum=0,avg; 
 
-    
     for(i=0;i<MAX;i++)
         sum+=a[i];
 
     avg=sum/MAX ; 
     return(avg); 
 }
-
-
+ 
 int main() {
     int i;
     double a[MAX]; 
-    
-    
+   
     for(i=0;i<MAX;i++){
         printf("請輸入第%d個浮點數:",i+1);
         scanf("%lf",&a[i]);
     }
-    
     
     printf("\n您輸入的陣列值如下\n");
     for(i=0;i<MAX;i++)
