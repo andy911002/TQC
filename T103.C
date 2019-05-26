f94eef274ab2e9cb19126e0f0246a4ee8ee9e834
andy911002 <48443745+andy911002@users.noreply.github.com>
2019-05-26 09:49:16 +0800

    Create T103.C

---
+++ T103.C
@@ -0,0 +1,15 @@
+T103
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () 
+{
+     int i=100, j=200;
+     double d ;
+     d=123.456;
+     printf("%d+%d=%d\n", i, j, i+j);
+     printf("d=%10.2lf\n", d);
+     return 0;
+}
+
